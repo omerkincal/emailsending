25e64d2d9b90c9f40f2d1dd0c9f05fd1d6bd2bc2
ÖMER KINCAL <202804010@ogr.cbu.edu.tr>
2024-01-01 12:41:42 +0300

    first commit

---
+++ src/main/java/org/omarkincal/mailsending/MailSendingApplication.java
@@ -0,0 +1,13 @@
+package org.omarkincal.mailsending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSendingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailSendingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/omarkincal/mailsending/MailSendingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.omarkincal.mailsending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSendingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
